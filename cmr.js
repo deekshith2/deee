7fe86dc9da0aab289dc7e191dc7d8bcf8355e974
deekshith <deekshithchinna2@gmail.com>
2019-12-24 11:33:40 +0530

    new file

--- cmr.js
+++ cmr.js
@@ -4,3 +4,5 @@ const port = process.env.PORT || 3000
 app.use(express.static('deee'))
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/deee/index.html'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
